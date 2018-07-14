eb5ad84a3cb73df11f9ec5e21a2febb7a1658b01
Giulio Vian <giuliovdev@hotmail.com>
2018-07-14 11:47:36 +0100

    Fixes #5 and support for extended version

--- hugo-task/Select-HugoVersion.ps1
+++ hugo-task/Select-HugoVersion.ps1
@@ -1,8 +1,12 @@
-function Select-HugoVersion {
+function Select-HugoVersion
+{
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
-        [string]$PreferredVersion)
+        [string] $PreferredVersion,
+        [Parameter(Mandatory = $false)]
+        [bool] $ExtendedVersion = $false
+    )
 
     Trace-VstsEnteringInvocation $MyInvocation
     try {
@@ -13,17 +17,26 @@ function Select-HugoVersion {
         if ($PreferredVersion -eq "latest") {
             $availableReleases = Invoke-GitHubAPI "/repos/gohugoio/hugo/releases?draft=false"
         } else {
-            $availableReleases = Invoke-GitHubAPI "/repos/gohugoio/hugo/releases/tags/v${PreferredVersion}"            
+            $availableReleases = Invoke-GitHubAPI "/repos/gohugoio/hugo/releases/tags/v${PreferredVersion}"
         }
-        $release = $availableReleases | where { (-not $_.draft) -and (-not $_.prerelease) } | select id,name -First 1
-        
+        $release = $availableReleases | where { (-not $_.draft) -and (-not $_.prerelease) } | select id,name,assets -First 1
+
         Write-Verbose "Found release $( $release.name )"
 
-        $assets = Invoke-GitHubAPI "/repos/gohugoio/hugo/releases/$( $release.id )/assets"
-        $win64hugo = $assets | where { $_.name -like '*Windows*64*' }
+        $win64hugo = $release.assets | where { $_.name -like '*Windows*64*' }
 
-        return @{ Version = $release.name; DownloadURL = $win64hugo.browser_download_url }
-        
+        # extended since 0.43
+        $DownloadURL = $win64hugo.browser_download_url
+        if ($DownloadURL -isnot [string]) {
+            if ($ExtendedVersion) {
+                $DownloadURL = $DownloadURL | where { $_ -like '*extended*' }
+            } else {
+                $DownloadURL = $DownloadURL | where { $_ -notlike '*extended*' }
+            }
+        }
+
+        return @{ Version = $release.name; DownloadURL = $DownloadURL }
+
     } finally {
         Trace-VstsLeavingInvocation $MyInvocation
     }
@@ -35,5 +48,14 @@ function Invoke-GitHubAPI($api) {
     [System.Net.ServicePointManager]::SecurityProtocol = @("Tls12")
     # stick with v3 API
     $x = Invoke-WebRequest -Uri "https://api.github.com${api}" -Headers @{"accept"="application/vnd.github.v3+json"} -UseBasicParsing
-    return $x.Content | ConvertFrom-Json    
+    return $x.Content | ConvertFrom-Json
 }
+
+<#
+Select-HugoVersion 'latest' $false
+Select-HugoVersion 'latest' $true
+Select-HugoVersion '0.43' $false
+Select-HugoVersion '0.43' $true
+Select-HugoVersion '0.31' $false
+Select-HugoVersion '0.31' $true
+#>
